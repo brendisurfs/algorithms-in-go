15b4044959bb8f39538d80f84a8035115a1cf03e
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-08-29 16:08:42 -0700

    added multiple solutions with varying time complexity
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- recursion/nth-fibonacci/main.go
+++ recursion/nth-fibonacci/main.go
@@ -5,21 +5,43 @@ import (
 )
 
 func main() {
-	nth := GetNthFib(9)
+	store := make(map[int]int)
+	store[0] = 0
+	store[1] = 1
+
+	nth := GetNthFib(9, store)
 	fmt.Println(nth)
+
+	iterNth := IterNthFib(9)
+	fmt.Println(iterNth)
 }
 
-func GetNthFib(n int) int {
-	store := make(map[int]int)
-
-	intSlice := []int{0, 1}
-
-	for i := 0; i <= n; i++ {
-		next := intSlice[0] + intSlice[1]
-		store[i] = next
-		intSlice[0] = intSlice[1]
-		intSlice[1] = next
+// O(n) time | O(n) space
+func GetNthFib(n int, store map[int]int) int {
+	_, found := store[n]
+	if found {
+		return store[n]
+	} else {
+		store[n] = GetNthFib(n-1, store) + GetNthFib(n-2, store)
+		return store[n]
 	}
+}
 
-	return store[n]
+// O(n) time | O(1) space
+func IterNthFib(n int) int {
+
+	lastTwo := []int{0, 1}
+	counter := 2
+
+	for counter <= n {
+		next := lastTwo[0] + lastTwo[1]
+		lastTwo[0] = lastTwo[1]
+		lastTwo[1] = next
+		counter += 1
+	}
+	if n > 1 {
+		return lastTwo[1]
+	} else {
+		return lastTwo[0]
+	}
 }
